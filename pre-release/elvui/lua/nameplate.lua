ccd5b0211ccea6b91a963544f7fe63193ffa5a97
yankikucuk <kendinikertenkelebek@me.com>
2024-08-06 20:00:35 +0300

    Updated

--- pre-release/elvui/lua/nameplate.lua
+++ pre-release/elvui/lua/nameplate.lua
@@ -1,157 +1,157 @@
 {
-  ["nameplates"] = {
-      ["filters"] = {
-          ["Bosses"] = {
-              ["triggers"] = {
-                  ["names"] = {
-                      ["186116"] = true,
-                      ["186120"] = true,
-                      ["186121"] = true,
-                      ["186122"] = true,
-                      ["186124"] = true,
-                      ["186125"] = true,
-                  },
-              },
-          },
-          ["Elite Mobs"] = {
-              ["triggers"] = {
-                  ["names"] = {
-                      ["187033"] = true,
-                  },
-              },
-          },
-          ["ElvUI_NonTarget"] = {
-              ["actions"] = {
-                  ["alpha"] = 100,
-                  ["scale"] = 0.75,
-              },
-          },
-          ["ElvUI_Target"] = {
-              ["actions"] = {
-                  ["alpha"] = 100,
-                  ["scale"] = 1.1,
-              },
-          },
-          ["Focused"] = {
-              ["actions"] = {
-                  ["texture"] = {
-                      ["enable"] = true,
-                      ["texture"] = "Cutting Edge Stripped (Plater)",
-                  },
-              },
-              ["triggers"] = {
-                  ["isFocus"] = true,
-              },
-          },
-          ["Important Casts"] = {
-              ["actions"] = {
-                  ["flash"] = {
-                      ["enable"] = true,
-                      ["speed"] = 5,
-                  },
-                  ["glow"] = {
-                      ["color"] = {
-                          [1] = 1,
-                          [2] = 0.9764706492424,
-                          [3] = 0,
-                          [4] = 0.89999997615814,
-                      },
-                      ["enable"] = true,
-                      ["lines"] = 10,
-                      ["size"] = 2,
-                      ["speed"] = 0.4,
-                  },
-              },
-              ["triggers"] = {
-                  ["casting"] = {
-                      ["spells"] = {
-                          ["382555"] = true,
-                          ["382556"] = true,
-                      },
-                  },
-              },
-          },
-          ["Interrupt Mobs"] = {
-              ["actions"] = {
-                  ["color"] = {
-                      ["health"] = true,
-                      ["healthColor"] = {
-                          ["g"] = 0.85098046064377,
-                          ["r"] = 0,
-                      },
-                  },
-              },
-              ["triggers"] = {
-                  ["names"] = {
-                      ["185528"] = true,
-                      ["185656"] = true,
-                      ["186191"] = true,
-                      ["186220"] = true,
-                      ["186246"] = true,
-                  },
-              },
-          },
-          ["Stealth Detect Mobs"] = {
-              ["actions"] = {
-                  ["color"] = {
-                      ["health"] = true,
-                      ["healthColor"] = {
-                          ["b"] = 0.78431379795074,
-                          ["r"] = 0,
-                      },
-                  },
-              },
-              ["triggers"] = {
-                  ["names"] = {
-                      ["185529"] = true,
-                      ["185691"] = true,
-                      ["186208"] = true,
-                      ["186226"] = true,
-                      ["186229"] = true,
-                      ["187192"] = true,
-                      ["195135"] = true,
-                  },
-              },
-          },
-          ["Stealth Mobs"] = {
-              ["actions"] = {
-                  ["color"] = {
-                      ["health"] = true,
-                      ["healthColor"] = {
-                          ["g"] = 0,
-                          ["r"] = 0.8666667342186,
-                      },
-                  },
-              },
-              ["triggers"] = {
-                  ["names"] = {
-                      ["186242"] = true,
-                      ["197857"] = true,
-                  },
-              },
-          },
-          ["Stun Mobs"] = {
-              ["actions"] = {
-                  ["color"] = {
-                      ["health"] = true,
-                      ["healthColor"] = {
-                          ["b"] = 0.73333334922791,
-                          ["g"] = 0,
-                      },
-                  },
-              },
-              ["triggers"] = {
-                  ["names"] = {
-                      ["185508"] = true,
-                      ["185534"] = true,
-                      ["187224"] = true,
-                      ["187315"] = true,
-                      ["194241"] = true,
-                      ["194487"] = true,
-                      ["194745"] = true,
-                  },
-              },
-          },
-      },
-  },
+    ["nameplates"] = {
+        ["filters"] = {
+            ["Bosses"] = {
+                ["triggers"] = {
+                    ["names"] = {
+                        ["186116"] = true,
+                        ["186120"] = true,
+                        ["186121"] = true,
+                        ["186122"] = true,
+                        ["186124"] = true,
+                        ["186125"] = true,
+                    },
+                },
+            },
+            ["Elite Mobs"] = {
+                ["triggers"] = {
+                    ["names"] = {
+                        ["187033"] = true,
+                    },
+                },
+            },
+            ["ElvUI_NonTarget"] = {
+                ["actions"] = {
+                    ["alpha"] = 100,
+                    ["scale"] = 0.75,
+                },
+            },
+            ["ElvUI_Target"] = {
+                ["actions"] = {
+                    ["alpha"] = 100,
+                    ["scale"] = 1.1,
+                },
+            },
+            ["Focused"] = {
+                ["actions"] = {
+                    ["texture"] = {
+                        ["enable"] = true,
+                        ["texture"] = "Cutting Edge Stripped (Plater)",
+                    },
+                },
+                ["triggers"] = {
+                    ["isFocus"] = true,
+                },
+            },
+            ["Important Casts"] = {
+                ["actions"] = {
+                    ["flash"] = {
+                        ["enable"] = true,
+                        ["speed"] = 5,
+                    },
+                    ["glow"] = {
+                        ["color"] = {
+                            [1] = 1,
+                            [2] = 0.9764706492424,
+                            [3] = 0,
+                            [4] = 0.89999997615814,
+                        },
+                        ["enable"] = true,
+                        ["lines"] = 10,
+                        ["size"] = 2,
+                        ["speed"] = 0.4,
+                    },
+                },
+                ["triggers"] = {
+                    ["casting"] = {
+                        ["spells"] = {
+                            ["382555"] = true,
+                            ["382556"] = true,
+                        },
+                    },
+                },
+            },
+            ["Interrupt Mobs"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0.85098046064377,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["185528"] = true,
+                        ["185656"] = true,
+                        ["186191"] = true,
+                        ["186220"] = true,
+                        ["186246"] = true,
+                    },
+                },
+            },
+            ["Stealth Detect Mobs"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["b"] = 0.78431379795074,
+                            ["r"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["185529"] = true,
+                        ["185691"] = true,
+                        ["186208"] = true,
+                        ["186226"] = true,
+                        ["186229"] = true,
+                        ["187192"] = true,
+                        ["195135"] = true,
+                    },
+                },
+            },
+            ["Stealth Mobs"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["g"] = 0,
+                            ["r"] = 0.8666667342186,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["186242"] = true,
+                        ["197857"] = true,
+                    },
+                },
+            },
+            ["Stun Mobs"] = {
+                ["actions"] = {
+                    ["color"] = {
+                        ["health"] = true,
+                        ["healthColor"] = {
+                            ["b"] = 0.73333334922791,
+                            ["g"] = 0,
+                        },
+                    },
+                },
+                ["triggers"] = {
+                    ["names"] = {
+                        ["185508"] = true,
+                        ["185534"] = true,
+                        ["187224"] = true,
+                        ["187315"] = true,
+                        ["194241"] = true,
+                        ["194487"] = true,
+                        ["194745"] = true,
+                    },
+                },
+            },
+        },
+    },
 }::styleFilters
