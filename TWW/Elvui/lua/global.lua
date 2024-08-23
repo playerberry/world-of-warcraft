ad4eaa54608fa62682f59ead447a5c2042b7aeba
yankikucuk <kendinikertenkelebek@me.com>
2024-08-23 23:38:50 +0300

    tww update

---
+++ TWW/Elvui/lua/global.lua
@@ -0,0 +1,35 @@
+{
+    ["datatexts"] = {
+        ["settings"] = {
+            ["Bags"] = {
+                ["NoLabel"] = true,
+            },
+            ["Combat"] = {
+                ["TimeFull"] = false,
+            },
+            ["Durability"] = {
+                ["percThreshold"] = 40,
+            },
+            ["Experience"] = {
+                ["textFormat"] = "PERCENT",
+            },
+            ["Friends"] = {
+                ["hideAFK"] = true,
+                ["hideApp"] = true,
+                ["hideBSAp"] = true,
+            },
+            ["Talent/Loot Specialization"] = {
+                ["iconOnly"] = true,
+            },
+        },
+    },
+    ["general"] = {
+        ["WorldMapCoordinates"] = {
+            ["position"] = "TOPLEFT",
+        },
+        ["fadeMapDuration"] = 0.1,
+        ["mapAlphaWhenMoving"] = 0.35,
+        ["smallerWorldMap"] = false,
+        ["smallerWorldMapScale"] = 1,
+    },
+}::global
